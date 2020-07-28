5d5d9b3f4755063016266307d4fca49f8ab3a52a
MariusS94 <marius1spomer@gmail.com>
2020-07-28 14:58:09 +0200

    Add JS function createButton to button

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,6 +1,18 @@
 import "./button.css";
-import button from "./button.html";
+/* import button from "./button.html"; */
 
 export default { title: "Button" };
 
-export const addToCart = () => button;
+function createButton(buttonText) {
+  const button = document.createElement("button");
+  button.classList = "btn";
+  const text = document.createTextNode(buttonText);
+  button.append(text);
+  return button;
+}
+
+export const addToCart = () => {
+  let button = createButton("Add to cart");
+
+  return button;
+};

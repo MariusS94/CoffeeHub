8ce23f71891025a7d9de38bf06a1572551f60e4f
MariusS94 <marius1spomer@gmail.com>
2020-07-28 14:56:07 +0200

    Add JS to quantitySelector

--- src/components/quantitySelector/quantitySelector.stories.js
+++ src/components/quantitySelector/quantitySelector.stories.js
@@ -1,6 +1,36 @@
 import "./quantitySelector.css";
 import quantitySelector from "./quantitySelector.html";
+import { useEffect } from "@storybook/client-api";
 
 export default { title: "Quantity Selector" };
 
-export const coffeeSelector = () => quantitySelector;
+/* export const coffeeSelector = () => {
+  return quantitySelector;
+}; */
+
+export const basic = () => {
+  useEffect(() => {
+    const minusButton = document.querySelector(".btn_one");
+    const plusButton = document.querySelector(".btn_two");
+    const resultElement = document.querySelector(".result_field");
+
+    minusButton.addEventListener("click", () => {
+      /* alert("minus") */
+      const oldResult = Number(resultElement.innerHTML);
+      if (oldResult === 2) {
+        minusButton.disabled = true;
+      }
+      resultElement.innerHTML = oldResult - 1;
+    });
+    plusButton.addEventListener("click", () => {
+      /* alert("plus") */
+      const oldResult = Number(resultElement.innerHTML);
+      if (oldResult === 1) {
+        minusButton.disabled = false;
+      }
+      resultElement.innerHTML = oldResult + 1;
+    });
+  });
+
+  return quantitySelector;
+};
